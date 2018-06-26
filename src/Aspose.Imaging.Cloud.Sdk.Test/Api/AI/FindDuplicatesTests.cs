29bc17ca98a86626df2f0fc4fb540fecf73b168c
Alexander Ermakov <alexander.ermakov@aspose.com>
2018-06-26 11:43:53 +0300

    Add tests

---
+++ src/Aspose.Imaging.Cloud.Sdk.Test/Api/AI/FindDuplicatesTests.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Aspose" file="ResizeApiTests.cs">
+//   Copyright (c) 2018 Aspose.Imaging for Cloud
+// </copyright>
+// <summary>
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Aspose.Imaging.Cloud.Sdk.Test.Api.AI
+{
+    using System.Net;
+    using Model.Requests;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FindDuplicatesTests : TestImagingAIBase
+    {
+        private const string ComparableImage = "ComparableImage.jpg";
+        private const string ComparingImageSimilarLess15 = "ComparingImageSimilar15.jpg";
+        private const string ComparingImageSimilarMore75 = "ComparingImageSimilar75.jpg";
+
+        [Test]
+        public void FindDuplicatesTest()
+        {
+            this.AddImageFeaturesToSearchContext($"{this.OriginalDataFolder}/FindSimilar", true);
+
+            var image = this.GetStoragePath(ComparableImage);
+            this.AddImageFeaturesToSearchContext(image);
+
+            image = this.GetStoragePath(ComparingImageSimilarLess15);
+            this.AddImageFeaturesToSearchContext(image);
+
+            image = this.GetStoragePath(ComparingImageSimilarMore75);
+            this.AddImageFeaturesToSearchContext(image);
+
+            var response = this.ImagingApi.GetSearchContextFindDuplicates(
+                new GetSearchContextFindDuplicatesRequest(this.SearchContextId, 80));
+            Assert.AreEqual(HttpStatusCode.OK, response.Code);
+            Assert.AreEqual(1, response.Duplicates.Count);
+        }
+    }
+}
---
+++ src/Aspose.Imaging.Cloud.Sdk.Test/Api/AI/TestImagingAIBase.cs
@@ -0,0 +1,97 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Aspose" file="ResizeApiTests.cs">
+//   Copyright (c) 2018 Aspose.Imaging for Cloud
+// </copyright>
+// <summary>
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Aspose.Imaging.Cloud.Sdk.Test.Api.AI
+{
+    using System.IO;
+    using System.Net;
+    using Model.Requests;
+    using NUnit.Framework;
+    using Storage.Cloud.Sdk.Model.Requests;
+
+    [TestFixture]
+    public abstract class TestImagingAIBase: ApiTester
+    {
+        [SetUp]
+        public void InitTest()
+        {
+
+            this.SearchContextId = this.CreateSearchContext();
+        }
+
+        [TearDown]
+        public void FinalizeTest()
+        {
+            if (!string.IsNullOrEmpty(this.SearchContextId))
+            {
+                this.DeleteSearchContext(this.SearchContextId);
+            }
+
+            if (this.StorageApi.GetIsExist(new GetIsExistRequest(TempFolder, null, DefaultStorage)).FileExist.IsExist.Value)
+            {
+                this.StorageApi.DeleteFolder(new DeleteFolderRequest(TempFolder, DefaultStorage, true));
+            }
+
+        }
+
+        protected string SearchContextId { get; private set; }
+
+        protected string OriginalDataFolder => "ImagingAI";
+
+        protected string TempFolder => "TempImagingAI";
+
+        protected string GetStoragePath(string imageName, string folder = null)
+        {
+            return Path.Combine(folder ?? this.OriginalDataFolder, imageName);
+        }
+
+        protected string CreateSearchContext()
+        {
+            var response = this.ImagingApi.PostCreateSearchContext(new PostCreateSearchContextRequest());
+            Assert.AreEqual(HttpStatusCode.OK, response.Code);
+            return response.Id;
+        }
+
+        protected void DeleteSearchContext(string searchContextId)
+        {
+            this.ImagingApi.DeleteSearchContext(new DeleteSearchContextRequest(searchContextId));
+        }
+
+        protected string GetSearchContextStatus(string searchContextId)
+        {
+            var response =  this.ImagingApi.GetSearchContextStatus(new GetSearchContextStatusRequest(this.SearchContextId));
+            Assert.AreEqual(HttpStatusCode.OK, response.Code);
+            return response.SearchStatus;
+        }
+
+        protected void AddImageFeaturesToSearchContext(string storageSourcePath, bool isFolder = false)
+        {
+            var request = isFolder
+                ? new PostSearchContextExtractImageFeaturesRequest(this.SearchContextId, imageId: null, folder: storageSourcePath)
+                : new PostSearchContextExtractImageFeaturesRequest(this.SearchContextId, imageId: storageSourcePath);
+            this.ImagingApi.PostSearchContextExtractImageFeatures(request);
+        }
+    }
+}
