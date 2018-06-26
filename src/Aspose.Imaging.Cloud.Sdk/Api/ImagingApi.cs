7eb5e4686cf4ad21c49079e75b66b23eddf16094
Maksym Shnurenok <maksym.shnurenok@aspose.com>
2018-06-26 14:35:15 +0300

    IMAGINGCLOUD-88 - Changed nuspec info, updated copyrights.

--- src/Aspose.Imaging.Cloud.Sdk/Api/ImagingApi.cs
+++ src/Aspose.Imaging.Cloud.Sdk/Api/ImagingApi.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright company="Aspose" file="ImagingApi.cs">
-//   Copyright (c) 2018 Aspose.Imaging for Cloud
+//   Copyright (c) 2018 Aspose Pty Ltd.
 // </copyright>
 // <summary>
 //   Permission is hereby granted, free of charge, to any person obtaining a copy
