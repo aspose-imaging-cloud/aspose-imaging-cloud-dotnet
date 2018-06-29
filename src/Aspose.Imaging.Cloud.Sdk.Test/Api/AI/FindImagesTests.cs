762f013a29c351bf11ce7866d46b97faa0741e5f
Alexander Ermakov <alexander.ermakov@aspose.com>
2018-06-29 16:59:01 +0300

    Updated copyright

--- src/Aspose.Imaging.Cloud.Sdk.Test/Api/AI/FindImagesTests.cs
+++ src/Aspose.Imaging.Cloud.Sdk.Test/Api/AI/FindImagesTests.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright company="Aspose" file="FindImagesTests.cs">
-//   Copyright (c) 2018 Copyright (c) 2018 Aspose Pty Ltd. All rights reserved.
+//  Copyright (c) 2018 Aspose Pty Ltd. All rights reserved.
 // </copyright>
 // <summary>
 //   Permission is hereby granted, free of charge, to any person obtaining a copy
