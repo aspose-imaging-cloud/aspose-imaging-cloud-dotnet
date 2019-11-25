d167282fbf371e8d1a516874bdbf0b50de0e6a48
Alexander Ermakov <alexander.ermakov@aspose.com>
2019-11-25 10:54:38 +0300

    Added user friendly message, when user did not assigned appSID and appKey, added exception handling

--- Examples/AsposeImagingCloudSDKExamples/ImagingExamples.cs
+++ Examples/AsposeImagingCloudSDKExamples/ImagingExamples.cs
@@ -34,153 +34,170 @@ namespace AsposeImagingCloudSDKExamples
     {
         static void Main(string[] args)
         {
-            string myAppKey = ""; // Get AppKey from https://dashboard.aspose.cloud/
             string myAppSid = ""; // Get AppSID from https://dashboard.aspose.cloud/
-            var api = new ImagingApi(appKey: myAppKey, appSid: myAppSid);
+            string myAppKey = ""; // Get AppKey from https://dashboard.aspose.cloud/
+          
+            if (string.IsNullOrEmpty(myAppSid) || string.IsNullOrEmpty(myAppKey))
+            {
+                Console.WriteLine("Please, initialize AppSid, AppKey with your valid credentials from https://dashboard.aspose.cloud/");
+                Console.WriteLine("If you haven't the credentials, please, Sign Up.");
+                Console.WriteLine("Press any key ...");
+                Console.ReadKey();
+                return;
+            }
 
-            PrepareOutput();
+            try
+            {
+                var api = new ImagingApi(appKey: myAppKey, appSid: myAppSid);
 
-            Console.WriteLine("Runnig Imaging Cloud examples:");
-            Console.WriteLine();
- 
-           // Update parameters of existing BMP image
-            UpdateBMPImage bmpImage = new UpdateBMPImage(api);
-            bmpImage.ModifyBmpFromStorage();
-            bmpImage.ModifyBmpAndUploadToStorage();
-            bmpImage.CreateModifiedBmpFromRequestBody();
+                PrepareOutput();
 
-            // Crop an existing image
-            CropImage cropImage = new CropImage(api);
-            cropImage.CropImageFromStorage();
-            cropImage.CropImageAndUploadToStorage();
-            cropImage.CreateCroppedImageFromRequestBody();
+                Console.WriteLine("Runnig Imaging Cloud examples:");
+                Console.WriteLine();
 
-            // Process existing EMF imaging using given parameters
-            UpdateEMFImage updateEMFImage = new UpdateEMFImage(api);
-            updateEMFImage.ModifyEmfFromStorage();
-            updateEMFImage.ModifyEmfAndUploadToStorage();
-            updateEMFImage.CreateModifiedEmfFromRequestBody();
+                // Update parameters of existing BMP image
+                UpdateBMPImage bmpImage = new UpdateBMPImage(api);
+                bmpImage.ModifyBmpFromStorage();
+                bmpImage.ModifyBmpAndUploadToStorage();
+                bmpImage.CreateModifiedBmpFromRequestBody();
 
-            // Export existing image to another format
-            ExportImage exportImage = new ExportImage(api);
-            exportImage.SaveImageAsFromStorage();
-            exportImage.SaveImageAsAndUploadToStorage();
-            exportImage.CreateSavedImageAsFromRequestBody();
+                // Crop an existing image
+                CropImage cropImage = new CropImage(api);
+                cropImage.CropImageFromStorage();
+                cropImage.CropImageAndUploadToStorage();
+                cropImage.CreateCroppedImageFromRequestBody();
 
-            // Get properties of an image
-            ImageProperties imageProperties = new ImageProperties(api);
-            imageProperties.GetImagePropertiesFromStorage();
-            imageProperties.ExtractImagePropertiesFromRequestBody();
+                // Process existing EMF imaging using given parameters
+                UpdateEMFImage updateEMFImage = new UpdateEMFImage(api);
+                updateEMFImage.ModifyEmfFromStorage();
+                updateEMFImage.ModifyEmfAndUploadToStorage();
+                updateEMFImage.CreateModifiedEmfFromRequestBody();
 
-            // Resize an existing image
-            ResizeImage resizeImage = new ResizeImage(api);
-            resizeImage.ResizeImageFromStorage();
-            resizeImage.ResizeImageAndUploadToStorage();
-            resizeImage.CreateResizedImageFromRequestBody();
+                // Export existing image to another format
+                ExportImage exportImage = new ExportImage(api);
+                exportImage.SaveImageAsFromStorage();
+                exportImage.SaveImageAsAndUploadToStorage();
+                exportImage.CreateSavedImageAsFromRequestBody();
 
-            // Rotate and/or flip an existing image
-            RotateFlipImage rotateFlipImage = new RotateFlipImage(api);
-            rotateFlipImage.RotateFlipImageFromStorage();
-            rotateFlipImage.RotateFlipImageAndUploadToStorage();
-            rotateFlipImage.CreateRotateFlippedImageFromRequestBody();
+                // Get properties of an image
+                ImageProperties imageProperties = new ImageProperties(api);
+                imageProperties.GetImagePropertiesFromStorage();
+                imageProperties.ExtractImagePropertiesFromRequestBody();
 
-            // TIFF Frames
-            TIFFFrames tiffFrames = new TIFFFrames(api);
-            // Get a specified frame from existing TIFF image
-            tiffFrames.GetImageFrameFromStorage();
-            // Get a specified frame from existing TIFF image, and upload the frame to Cloud Storage
-            tiffFrames.GetImageFrameAndUploadToStorage();
-            // Resize a TIFF frame
-            tiffFrames.ResizeImageFrameFromStorage();
-            // Crop a TIFF frame
-            tiffFrames.CropImageFrameFromStorage();
-            // RotateFlip a TIFF frame
-            tiffFrames.RotateFlipImageFrameFromStorage();
-            // Get other frames from existing TIFF image
-            tiffFrames.GetAllImageFramesFromStorage();
-            // Get separate frame from existing TIFF image.
-            // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
-            tiffFrames.CreateImageFrameFromRequestBody();
-            // Get separate frame properties of existing TIFF image
-            tiffFrames.GetImageFramePropertiesFromStorage();
-            // Get separate frame properties of existing TIFF image.
-            // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
-            tiffFrames.ExtractImageFramePropertiesFromRequestBody();
+                // Resize an existing image
+                ResizeImage resizeImage = new ResizeImage(api);
+                resizeImage.ResizeImageFromStorage();
+                resizeImage.ResizeImageAndUploadToStorage();
+                resizeImage.CreateResizedImageFromRequestBody();
 
-            // Update parameters of existing TIFF image
-            TIFFImage tiffImage = new TIFFImage(api);
-            tiffImage.ModifyTiffFromStorage();
-            tiffImage.ModifyTiffAndUploadToStorage();
-            tiffImage.CreateModifiedTiffFromRequestBody();
-            tiffImage.ConvertTiffToFaxFromStorage();
-            tiffImage.AppendTiffFromStorage();
+                // Rotate and/or flip an existing image
+                RotateFlipImage rotateFlipImage = new RotateFlipImage(api);
+                rotateFlipImage.RotateFlipImageFromStorage();
+                rotateFlipImage.RotateFlipImageAndUploadToStorage();
+                rotateFlipImage.CreateRotateFlippedImageFromRequestBody();
 
-            // Update parameters of existing GIF image
-            UpdateGIFImage updateGIFImage = new UpdateGIFImage(api);
-            updateGIFImage.ModifyGifFromStorage();
-            updateGIFImage.ModifyGifAndUploadToStorage();
-            updateGIFImage.CreateModifiedGifFromRequestBody();
+                // TIFF Frames
+                TIFFFrames tiffFrames = new TIFFFrames(api);
+                // Get a specified frame from existing TIFF image
+                tiffFrames.GetImageFrameFromStorage();
+                // Get a specified frame from existing TIFF image, and upload the frame to Cloud Storage
+                tiffFrames.GetImageFrameAndUploadToStorage();
+                // Resize a TIFF frame
+                tiffFrames.ResizeImageFrameFromStorage();
+                // Crop a TIFF frame
+                tiffFrames.CropImageFrameFromStorage();
+                // RotateFlip a TIFF frame
+                tiffFrames.RotateFlipImageFrameFromStorage();
+                // Get other frames from existing TIFF image
+                tiffFrames.GetAllImageFramesFromStorage();
+                // Get separate frame from existing TIFF image.
+                // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
+                tiffFrames.CreateImageFrameFromRequestBody();
+                // Get separate frame properties of existing TIFF image
+                tiffFrames.GetImageFramePropertiesFromStorage();
+                // Get separate frame properties of existing TIFF image.
+                // Image data is passed as zero-indexed multipart/form-data content or as raw body stream
+                tiffFrames.ExtractImageFramePropertiesFromRequestBody();
 
-            // Perform scaling, cropping and flipping of an existing image in a single request
-            UpdateImage updateImage = new UpdateImage(api);
-            updateImage.UpdateImageFromStorage();
-            updateImage.UpdateImageAndUploadToStorage();
-            updateImage.CreateUpdatedImageFromRequestBody();
+                // Update parameters of existing TIFF image
+                TIFFImage tiffImage = new TIFFImage(api);
+                tiffImage.ModifyTiffFromStorage();
+                tiffImage.ModifyTiffAndUploadToStorage();
+                tiffImage.CreateModifiedTiffFromRequestBody();
+                tiffImage.ConvertTiffToFaxFromStorage();
+                tiffImage.AppendTiffFromStorage();
 
-            // Update parameters of existing JPEG2000 image
-            UpdateJPEG2000Image updateJPEG2000Image = new UpdateJPEG2000Image(api);
-            updateJPEG2000Image.ModifyJpeg2000FromStorage();
-            updateJPEG2000Image.ModifyJpeg2000AndUploadToStorage();
-            updateJPEG2000Image.CreateModifiedJpeg2000FromRequestBody();
+                // Update parameters of existing GIF image
+                UpdateGIFImage updateGIFImage = new UpdateGIFImage(api);
+                updateGIFImage.ModifyGifFromStorage();
+                updateGIFImage.ModifyGifAndUploadToStorage();
+                updateGIFImage.CreateModifiedGifFromRequestBody();
 
-            // Update parameters of existing JPEG image
-            UpdateJPEGImage updateJPEGImage = new UpdateJPEGImage(api);
-            updateJPEGImage.ModifyJpegFromStorage();
-            updateJPEGImage.ModifyJpegAndUploadToStorage();
-            updateJPEGImage.CreateModifiedJpegFromRequestBody();
+                // Perform scaling, cropping and flipping of an existing image in a single request
+                UpdateImage updateImage = new UpdateImage(api);
+                updateImage.UpdateImageFromStorage();
+                updateImage.UpdateImageAndUploadToStorage();
+                updateImage.CreateUpdatedImageFromRequestBody();
 
-            // Update parameters of existing PSD image
-            UpdatePSDImage updatePSDImage = new UpdatePSDImage(api);
-            updatePSDImage.ModifyPsdFromStorage();
-            updatePSDImage.ModifyPsdAndUploadToStorage();
-            updatePSDImage.CreateModifiedPsdFromRequestBody();
+                // Update parameters of existing JPEG2000 image
+                UpdateJPEG2000Image updateJPEG2000Image = new UpdateJPEG2000Image(api);
+                updateJPEG2000Image.ModifyJpeg2000FromStorage();
+                updateJPEG2000Image.ModifyJpeg2000AndUploadToStorage();
+                updateJPEG2000Image.CreateModifiedJpeg2000FromRequestBody();
 
-            // Update parameters of existing WEBP image
-            WEBPImage webpImage = new WEBPImage(api);
-            webpImage.ModifyWebPFromStorage();
-            webpImage.ModifyWebPAndUploadToStorage();
-            webpImage.CreateModifiedWebPFromRequestBody();
+                // Update parameters of existing JPEG image
+                UpdateJPEGImage updateJPEGImage = new UpdateJPEGImage(api);
+                updateJPEGImage.ModifyJpegFromStorage();
+                updateJPEGImage.ModifyJpegAndUploadToStorage();
+                updateJPEGImage.CreateModifiedJpegFromRequestBody();
 
-            // Process existing WMF image using given parameters
-            WMFImage wmfImage = new WMFImage(api);
-            wmfImage.ModifyWmfFromStorage();
-            wmfImage.ModifyWmfAndUploadToStorage();
-            wmfImage.CreateModifiedWmfFromRequestBody();
+                // Update parameters of existing PSD image
+                UpdatePSDImage updatePSDImage = new UpdatePSDImage(api);
+                updatePSDImage.ModifyPsdFromStorage();
+                updatePSDImage.ModifyPsdAndUploadToStorage();
+                updatePSDImage.CreateModifiedPsdFromRequestBody();
 
-            // AI APIs           
+                // Update parameters of existing WEBP image
+                WEBPImage webpImage = new WEBPImage(api);
+                webpImage.ModifyWebPFromStorage();
+                webpImage.ModifyWebPAndUploadToStorage();
+                webpImage.CreateModifiedWebPFromRequestBody();
 
-            Console.WriteLine("Runnig AI examples:");
-            Console.WriteLine();
+                // Process existing WMF image using given parameters
+                WMFImage wmfImage = new WMFImage(api);
+                wmfImage.ModifyWmfFromStorage();
+                wmfImage.ModifyWmfAndUploadToStorage();
+                wmfImage.CreateModifiedWmfFromRequestBody();
 
-            // Compare two images
-            CompareImages compareImages = new CompareImages(api);
-            compareImages.PrepareSearchContext();
-            compareImages.CompareTwoImagesInCloud();
-            compareImages.CompareLoadedImageToImageInCloud();
-            compareImages.DeleteSearchContext();
+                // AI APIs           
 
-            // Find Duplicate Images
-            FindDuplicateImages findDuplicateImages = new FindDuplicateImages(api);
-            findDuplicateImages.PrepareSearchContext();
-            findDuplicateImages.FindImageDuplicates();
-            findDuplicateImages.DeleteSearchContext();
+                Console.WriteLine("Runnig AI examples:");
+                Console.WriteLine();
 
-            // Find Similar Images
-            FindImages findImages = new FindImages(api);
-            findImages.PrepareSearchContext();
-            findImages.FindSimilarImages();
-            findImages.FindImagesByTag();
-            findImages.DeleteSearchContext();
+                // Compare two images
+                CompareImages compareImages = new CompareImages(api);
+                compareImages.PrepareSearchContext();
+                compareImages.CompareTwoImagesInCloud();
+                compareImages.CompareLoadedImageToImageInCloud();
+                compareImages.DeleteSearchContext();
+
+                // Find Duplicate Images
+                FindDuplicateImages findDuplicateImages = new FindDuplicateImages(api);
+                findDuplicateImages.PrepareSearchContext();
+                findDuplicateImages.FindImageDuplicates();
+                findDuplicateImages.DeleteSearchContext();
+
+                // Find Similar Images
+                FindImages findImages = new FindImages(api);
+                findImages.PrepareSearchContext();
+                findImages.FindSimilarImages();
+                findImages.FindImagesByTag();
+                findImages.DeleteSearchContext();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Something goes wrong: {ex}");
+            }
 
             Console.WriteLine("Press any key ...");
             Console.ReadKey();
