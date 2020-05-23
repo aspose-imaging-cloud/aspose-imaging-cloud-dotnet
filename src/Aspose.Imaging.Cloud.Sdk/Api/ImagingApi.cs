8120b2b81583b0d803f26a5416e9a7927be36e8e
aspose-imaging-cloud-deployer <alexander.feigin@aspose.com>
2020-05-23 04:18:10 -0500

    SDK regenerated by CI server

--- src/Aspose.Imaging.Cloud.Sdk/Api/ImagingApi.cs
+++ src/Aspose.Imaging.Cloud.Sdk/Api/ImagingApi.cs
@@ -2015,13 +2015,8 @@ namespace Aspose.Imaging.Cloud.Sdk.Api
                 null, 
                 null, 
                 formParams);
+            return response;
             
-            if (response == null)
-            {
-                return null;
-            }
-      
-            return (System.IO.Stream)SerializationHelper.Deserialize<System.IO.Stream>(StreamHelper.ToString(response));
         }
         
         /// <summary>
@@ -3852,6 +3847,12 @@ namespace Aspose.Imaging.Cloud.Sdk.Api
                 throw new ApiException(400, "Missing required parameter 'path' when calling UploadFile");
             }
 
+            // verify the required parameter 'file' is set
+            if (request.File == null) 
+            {
+                throw new ApiException(400, "Missing required parameter 'file' when calling UploadFile");
+            }
+
             // create path and map variables
             var resourcePath = this.Configuration.GetApiRootUrl() + "/imaging/storage/file/{path}";
             resourcePath = Regex
@@ -3862,6 +3863,10 @@ namespace Aspose.Imaging.Cloud.Sdk.Api
             resourcePath = UrlHelper.AddPathParameter(resourcePath, "path", request.path);
             resourcePath = UrlHelper.AddQueryParameterToUrl(resourcePath, "storageName", request.storageName);
             
+            if (request.File != null) 
+            {
+                formParams.Add("file", this.apiInvoker.ToFileInfo(request.File, "File"));
+            }
             var response = this.apiInvoker.InvokeApi(
                 resourcePath, 
                 "PUT", 
