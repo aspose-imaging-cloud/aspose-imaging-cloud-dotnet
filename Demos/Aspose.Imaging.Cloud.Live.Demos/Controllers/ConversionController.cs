4feb3650e745e7fccaf43d1d06bf77ee7c3b2e47
Shakeel Faiz <shakeelfaiz2@gmail.com>
2021-07-15 18:00:52 +0500

    Converted Aspose.Imaging Cloud Conversion App to .NET 5.

---
+++ Demos/Aspose.Imaging.Cloud.Live.Demos/Controllers/ConversionController.cs
@@ -0,0 +1,69 @@
+﻿using Aspose.Imaging.Cloud.Live.Demos.Models;
+using Aspose.Imaging.Cloud.Live.Demos.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Aspose.Imaging.Cloud.Live.Demos.Controllers
+{
+    public class ConversionController : BaseController
+    {
+        public ConversionController(IMemoryCache cache, IAsposeImagingCloudApiService imagingService) : base(cache)
+        {
+            ImagingService = imagingService;
+        }
+
+        public override string Product => (string)RouteData.Values["product"];
+
+        public IAsposeImagingCloudApiService ImagingService { get; }
+
+        public IActionResult Conversion()
+        {
+            var model = new ViewModel(this, nameof(Conversion))
+            {
+                SaveAsComponent = true,
+                MaximumUploadFiles = 1,
+                UseSorting = false
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<Response> Conversion(string outputType)
+        {
+            IFormFile postedFile = Request.Form.Files[0];
+            string fileName = postedFile.FileName;
+            string fileData = string.Empty;
+            string outputFileName = string.Empty;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await postedFile.CopyToAsync(ms);
+                ms.Position = 0;
+
+                string fromFormat = Path.GetExtension(fileName).Substring(1);
+                string toFormat = outputType;
+                var file = ms;
+                outputFileName = Path.GetFileNameWithoutExtension(fileName) + "." + outputType;
+
+                using (MemoryStream convertResult = ImagingService.Convert(file, fileName, toFormat) as MemoryStream)
+                {
+                    fileData = Convert.ToHexString(convertResult.ToArray());
+                }
+            }
+
+            return new Response
+            {
+                StatusCode = 200,
+                FileName = outputFileName,
+                FileData = fileData
+            };
+        }
+
+    }
+}
--- Demos/README.md
+++ Demos/README.md
@@ -2,3 +2,9 @@
 
 Aspose.Imaging Cloud for .NET Live Demos Source Code.
  
+# How to run
+ 
+ 1. Clone the repository.
+ 2. Open the solution file in the latest version of Microsoft Visual Studio.
+ 3. Set **Client Id** and **Client Secret** in **appsettings.json** file.
+ 4. You can get the Client Id and Client Secret from this URL: https://dashboard.aspose.cloud
